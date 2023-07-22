fd8c7711c36d09ad2446090eaa81c185e71ce627
Joshua Baker <joshuadfbaker@gmail.com>
2023-07-21 19:00:14 -0700

    counter function finished

--- public/scripts/counter.js
+++ public/scripts/counter.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
 
+  $(".form__textarea").on('input', onInput);
 
 
-  
 });
+
+const onInput = function(event) {
+
+  let $input = $(this);
+
+  let len = $input.val().length;
+  let charsLeft = 10 - len; // reassign to 140
+
+  const $counter = $input.closest('form').find('.count');
+
+  $counter.text(charsLeft);
+  $counter.toggleClass('form--red', charsLeft < 0);
+};
