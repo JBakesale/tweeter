c813be02b8b4d8215936887f45ca52864347469f
Joshua Baker <joshuadfbaker@gmail.com>
2023-07-24 04:08:14 -0700

    hardcoded createTweetElement func for dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,40 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function () {
+  loadTweets();
+});
+
+
+const loadTweets = () => {
+
+  // send ajax request
+  $.get('/tweets')
+  .then(data => {
+    renderTweets(data);
+  })
+}
+
+
+// Loop through the tweets and dynamically render each
+const renderTweets = (tweets) => {
+  const container = $('.tweets');
+  container.empty();
+
+  tweets.forEach(function(tweet) {
+    let tweetElement = createTweetElement(tweet);
+    container.append(tweetElement); //prepend
+  })
+
+};
+
+// Create the tweet HTML
+const createTweetElement = (tweetData) => {
+  let $tweet = $('<article>').addClass('tweet');
+
+  let html = `
+    <header class="top"
+      <div>
+        <img class="tweet__avatar" src="${tweetData.user.avatar}">
+        `
+}
